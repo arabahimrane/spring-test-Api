e3347bcc1f13cfa14bdff1e241c7aba8e5610100
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-08-05 17:45:27 -0400

    README

--- src/test/java/com/test/testApi/web/EndToEndTest.java
+++ src/test/java/com/test/testApi/web/EndToEndTest.java
@@ -1,7 +1,6 @@
 package com.test.testApi.web;
 
 import org.assertj.core.api.Assertions;
-import org.h2.engine.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
